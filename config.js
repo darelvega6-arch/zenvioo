4af0cf4767383ac9b82a56bfeb4aacadc864160b
darelvega6-arch <darelvega6@icloud.com>
2025-11-06 08:51:36 +0000

    Configurar AWS S3

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Configuración de API
-const API_URL = 'https://TU_API_GATEWAY_URL/prod/api';
+const API_URL = 'https://main.d3a8pfy9udfjd3.amplifyapp.com/api';
 
 // Exportar para uso en otros archivos
 if (typeof module !== 'undefined' && module.exports) {
